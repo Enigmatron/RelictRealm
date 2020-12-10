ffba2e604fecb23ed3295e4b505466c830a508a0
Enigmatron <idriesk@gmail.com>
2020-12-09 18:42:24 -0600

    Partial Active Entity Success: open unity to fix file name changes

---
+++ Assets/Scripts/Entity Scripts/Abstract Entity Partials/ActiveEntity.CC.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+
+
+public abstract partial class ActiveEntity {
+
+
+
+
+
+
+
+
+}
--- Assets/Scripts/Entity Scripts/EntityTemplate.cs
+++ Assets/Scripts/Entity Scripts/EntityTemplate.cs
@@ -10,6 +10,14 @@ using System;
 
 public abstract partial class ActiveEntity : Entity
 {
+    protected partial void OnEntityEnter(Collider other);
+     protected partial void OnEnemyStay(Collider other);
+    protected partial void OnEntityExit(Collider other);
+    public partial bool entityVisionCalc(ActiveEntity other);
+    public partial void EntityDectionEventCall();
+    public partial bool isEntityNeutral(ActiveEntity other);
+    public partial bool isEntityAlly(ActiveEntity other);
+    public partial bool isEntityEnemy(ActiveEntity other);
     public enum TeamValue
     {
         Player1,
@@ -66,6 +74,8 @@ public abstract partial class ActiveEntity : Entity
         controller.Move(MoveVector * Time.deltaTime);
     }
 
+
+
     #endregion
     protected virtual void Awake()
     {
@@ -88,327 +98,11 @@ public abstract partial class ActiveEntity : Entity
     {
 
     }
-
-    #region EntityDetection
-
-    //TODO: add the combat timer: a timer that resets on damage taken; the section is gonna be indeterminant for now;
-    SphereCollider detectionSphere;
-    public bool isNearVisibleEnemy
-    {
-        get;
-        protected set;
-    }
-    public bool isNearEnemy
-    {
-        get;
-        protected set;
-    }
-    public bool isNearVisibleAlly
-    {
-        get;
-        protected set;
-    }
-    public bool isNearAlly
-    {
-        get;
-        protected set;
-    }
-    public bool isNearVisibleNeutral
-    {
-        get;
-        protected set;
-    }
-    public bool isNearNeutral
-    {
-        get;
-        protected set;
-    }
-
-    public bool Combat
-    {
-        get
-        {
-            return isNearEnemy || Damage_Timer.Finished;
-        }
-    }
-
-    //this is for the out of combat system
-    ProgressionStateMachine damage_timer;
-
-    public ProgressionStateMachine Damage_Timer
-    {
-        get
-        {
-            return damage_timer;
-        }
-    }
-
-    // Bit shift the index of the layer (8) to get a bit mask
-    int layerMask = 1 << 8;
-
-    RaycastHit entityDetectionHit;
-
-
-    [Flags]
-    public enum EntityDetectionFlag : byte
-    {
-        onEnter = 1,
-        onStay = 2,
-        onExit = 4,
-        onInVision = 8, //gets turned off on stay
-        OnVisionStay = 16,
-        OnOutVision = 32, //gets turned off
-    }
-    //replace the events below
-    //the int is a flag for what event is being called whether it is true or not;
-    //ActiveEntity is the subject of the event;
-    //The events need to decern what teamvalue the entity has
-    //holds a list of methods that check the team value and flag within the UnityAction for these UnityEvents
-    List<UnityEvent<Dictionary<ActiveEntity, int>>> EntityDetectionEvents;
-
-    /* leaving this here to hellp me remember what events need to happen
-    // public UnityEvent<List<ActiveEntity>> onCombatEvent { get; protected set; }
-    // public UnityEvent<List<ActiveEntity>> onEnemyTriggerEnter { get; protected set; }
-    // public UnityEvent<List<ActiveEntity>> onAllyTriggerEnter { get; protected set; }
-
-    // public UnityEvent<List<ActiveEntity>> onEnemyTriggerExit { get; protected set; }
-    // public UnityEvent<List<ActiveEntity>> onAllyTriggerExit { get; protected set; }
-    // public UnityEvent<List<ActiveEntity>> onEnemyInVision { get; protected set; }
-    // public UnityEvent<List<ActiveEntity>> onAllyInVision { get; protected set; }
-    // public UnityEvent<List<ActiveEntity>> onEnemyOutVision { get; protected set; }
-    // public UnityEvent<List<ActiveEntity>> onAllyOutVision { get; protected set; }
-
-    // public UnityEvent<List<ActiveEntity>> onOutOfCombat { get; protected set; }
-    */
-
-
-    //uses the same flag as EntityDectionEvents
-    //the flag can tell you if the entity is in vision
-    //these are kept seperate as to maintain the isnearvariable for both to quickly get out of combat
-    public Dictionary<ActiveEntity, int> nearAllies;
-    //neutral is
-    public Dictionary<ActiveEntity, int> nearEnemies;
-    public Dictionary<ActiveEntity, int> nearNeutrals;
-
-
-    public bool isEntityAlly(ActiveEntity other)
-    {
-        return other.team == team;
-    }
-    public bool isEntityEnemy(ActiveEntity other)
-    {
-        return other.team != team && other.team != TeamValue.Neutral;
-    }
-    public bool isEntityNeutral(ActiveEntity other)
-    {
-        return other.team == TeamValue.Neutral;
-    }
-
-
-    //TODO: has to be called on fixed update
-    public void EntityDectionEventCall()
-    {
-        //placed here to see
-        //Dictionary< EntityDetectionFlag, List<UnityEvent<Dictionary<ActiveEntity, int>>>> EntityDetectionEvents;
-        foreach (UnityEvent<Dictionary<ActiveEntity, int>> y in EntityDetectionEvents)
-        {
-            y.Invoke(nearAllies);
-            y.Invoke(nearEnemies);
-            y.Invoke(nearNeutrals);
-            foreach(KeyValuePair<ActiveEntity, int> x in nearAllies){
-                if(MathLambda.FlagCheck(x.Value, (int)EntityDetectionFlag.onExit)){
-                    nearAllies.Remove(x.Key);
-                    continue;
-                }
-                if(MathLambda.FlagCheck(x.Value, (int)EntityDetectionFlag.OnOutVision)){
-                    MathLambda.FlagOff(x.Value, (int)EntityDetectionFlag.OnOutVision+(int)EntityDetectionFlag.onInVision+(int)EntityDetectionFlag.OnVisionStay);
-                }
-            }
-            foreach(KeyValuePair<ActiveEntity, int> x in nearEnemies){
-                if(MathLambda.FlagCheck(x.Value, (int)EntityDetectionFlag.onExit)){
-                    nearEnemies.Remove(x.Key);
-                    continue;
-                }
-                if(MathLambda.FlagCheck(x.Value, (int)EntityDetectionFlag.OnOutVision)){
-                    MathLambda.FlagOff(x.Value, (int)EntityDetectionFlag.OnOutVision+(int)EntityDetectionFlag.onInVision+(int)EntityDetectionFlag.OnVisionStay);
-                }
-            }
-            foreach(KeyValuePair<ActiveEntity, int> x in nearNeutrals){
-                if(MathLambda.FlagCheck(x.Value, (int)EntityDetectionFlag.onExit)){
-                    nearNeutrals.Remove(x.Key);
-                    continue;
-                }
-                if(MathLambda.FlagCheck(x.Value, (int)EntityDetectionFlag.OnOutVision)){
-                    MathLambda.FlagOff(x.Value, (int)EntityDetectionFlag.OnOutVision+(int)EntityDetectionFlag.onInVision+(int)EntityDetectionFlag.OnVisionStay);
-                }
-            }
-        }
-
-
-    }
-
-    //TODO: write the code, use ray cast and check the head, upper legs, and body and return tru if any are 'seen'
-    public bool entityVisionCalc(ActiveEntity other)
-    {
-        return true;
-    }
-
-    //add to a dictionary of near enemies and detect if they're in vision
-    //if: in vision then: add to in vision entities of enemies or allies;
-    //if: not in vision then: add enemies and allies to dictionary of such;
-    protected void OnEntityEnter(Collider other)
-    {
-        ActiveEntity ent = other.GetComponent<ActiveEntity>();
-        if (ent != null)
-        {
-            if (isEntityEnemy(ent))
-            {
-                nearEnemies.Add(ent, (int)EntityDetectionFlag.onEnter);
-                isNearEnemy = true;
-            }
-            if (isEntityAlly(ent))
-            {
-                nearAllies.Add(ent, (int)EntityDetectionFlag.onEnter);
-                isNearAlly = true;
-            }
-            if (isEntityNeutral(ent))
-            {
-                nearNeutrals.Add(ent, (int)EntityDetectionFlag.onEnter);
-                isNearNeutral = true;
-            }
-        }
-    }
-
-    protected void OnEntityExit(Collider other)
-    {
-
-        ActiveEntity ent = other.GetComponent<ActiveEntity>();
-        if (ent != null)
-        {
-            //TODO: add the appropriate flag
-            if (isEntityAlly(ent))
-            {
-                nearAllies[ent] = MathLambda.FlagOn(nearAllies[ent], (int)EntityDetectionFlag.onExit);
-                nearAllies[ent] = MathLambda.FlagOff(nearAllies[ent], (int)EntityDetectionFlag.OnVisionStay);
-                nearAllies[ent] = MathLambda.FlagOn(nearAllies[ent], (int)EntityDetectionFlag.OnOutVision);
-                if (nearAllies.Count == 0)
-                {
-                    isNearAlly = false;
-                    isNearVisibleAlly = false;
-                }
-                else
-                {
-
-                    foreach (KeyValuePair<ActiveEntity, int> item in nearAllies)
-                    {
-                        if (
-                            MathLambda.FlagCheck(item.Value, (int)EntityDetectionFlag.OnVisionStay+(int)EntityDetectionFlag.onInVision)
-                        )
-                        {
-                            isNearVisibleAlly = true;
-                            break;
-                        }
-                    }
-                }
-
-            }
-            if (isEntityEnemy(ent))
-            {
-                nearEnemies[ent] = MathLambda.FlagOn(nearEnemies[ent], (int)EntityDetectionFlag.onExit);
-                nearEnemies[ent] = MathLambda.FlagOff(nearEnemies[ent], (int)EntityDetectionFlag.OnVisionStay);
-                nearEnemies[ent] = MathLambda.FlagOn(nearEnemies[ent], (int)EntityDetectionFlag.OnOutVision);
-                isNearEnemy = nearEnemies.Count == 0 ? false : true;
-                if (nearEnemies.Count == 0)
-                {
-                    isNearEnemy = false;
-                    isNearVisibleEnemy = false;
-                }
-                else
-                {
-
-                    foreach (KeyValuePair<ActiveEntity, int> item in nearEnemies)
-                    {
-                        if (
-                            MathLambda.FlagCheck(item.Value, (int)EntityDetectionFlag.OnVisionStay+(int)EntityDetectionFlag.onInVision)
-                        )
-                        {
-                            isNearVisibleEnemy = true;
-                            break;
-                        }
-                    }
-                }
-            }
-            if (isEntityNeutral(ent))
-            {
-                nearNeutrals[ent] = MathLambda.FlagOn(nearNeutrals[ent], (int)EntityDetectionFlag.onExit);
-                nearNeutrals[ent] = MathLambda.FlagOff(nearNeutrals[ent], (int)EntityDetectionFlag.OnVisionStay);
-                nearNeutrals[ent] = MathLambda.FlagOn(nearNeutrals[ent], (int)EntityDetectionFlag.OnOutVision);
-                isNearNeutral = nearNeutrals.Count == 0 ? false : true;
-                if (nearNeutrals.Count == 0)
-                {
-                    isNearNeutral = false;
-                    isNearVisibleNeutral = false;
-                }
-                else
-                {
-
-                    foreach (KeyValuePair<ActiveEntity, int> item in nearNeutrals)
-                    {
-                        if (
-                            MathLambda.FlagCheck(item.Value, (int)EntityDetectionFlag.OnVisionStay+(int)EntityDetectionFlag.onInVision)
-                        )
-                        {
-                            isNearVisibleNeutral = true;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    protected void OnEnemyStay(Collider other)
-    {
-        ActiveEntity ent = other.GetComponent<ActiveEntity>();
-        if (ent != null)
-        {
-            if (isEntityAlly(ent))
-            {
-                nearAllies[ent] = MathLambda.FlagOn(nearAllies[ent], (int)EntityDetectionFlag.onStay);
-            }
-            if (isEntityEnemy(ent))
-            {
-                nearEnemies[ent] = MathLambda.FlagOn(nearEnemies[ent], (int)EntityDetectionFlag.onStay);
-            }
-            if (isEntityNeutral(ent))
-            {
-                nearNeutrals[ent] = MathLambda.FlagOn(nearNeutrals[ent], (int)EntityDetectionFlag.onStay);
-            }
-
-            if (entityVisionCalc(ent))
-            {
-                if (isEntityAlly(ent))
-                {
-                    nearAllies[ent] = MathLambda.FlagOn(nearAllies[ent], (int)EntityDetectionFlag.OnVisionStay);
-                }
-                if (isEntityEnemy(ent))
-                {
-                    nearEnemies[ent] = MathLambda.FlagOn(nearEnemies[ent], (int)EntityDetectionFlag.OnVisionStay);
-                }
-                if (isEntityNeutral(ent))
-                {
-                    nearNeutrals[ent] = MathLambda.FlagOn(nearNeutrals[ent], (int)EntityDetectionFlag.OnVisionStay);
-                }
-            }
-        }
-    }
-
-    #endregion
     protected virtual void OnTriggerEnter(Collider other)
     {
         OnEntityEnter(other);
     }
-    
+
     protected virtual void OnTriggerStay(Collider other)
     {
         OnEnemyStay(other);
