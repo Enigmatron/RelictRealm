8bbf41dd0927d814e3aa04615eaa868285364928
Enigmatron <idriesk@gmail.com>
2020-12-09 18:51:15 -0600

    Moved Movement to partial

--- Assets/Scripts/Entity Scripts/Abstract Entity Partials/ActiveEntity.Movement.cs
+++ Assets/Scripts/Entity Scripts/Abstract Entity Partials/ActiveEntity.Movement.cs
@@ -7,7 +7,50 @@ using System;
 
 
 public abstract partial class ActiveEntity {
+    
+    public float testmovespeed = 12;
+    public float testjumpspeed = 20;
+    public string backwardaxis = "Vertical";
+    protected Vector3 MoveVector = Vector3.zero;
+    protected float currAcceleration;
+    public float gravity = 32.2F;
+    protected CharacterController controller;
+    protected float acclX;
+    protected float acclZ;
+    protected partial void Movement(Vector3 Movement)
+    {
+        //TODO: Use The entity's profile
+        float movespeed = testmovespeed;
+        float jumpspeed = testjumpspeed;
+        float backwardMovement = Input.GetAxisRaw(backwardaxis) == -1f ? 0.4f : 1f;
 
+        //side to side movement
+        MoveVector.x = Movement.x * movespeed;
+        //forward back movement
+        MoveVector.z = Movement.z * movespeed;
+
+        if (controller.isGrounded)
+        {
+            if (Movement.y > 0)
+            {
+                MoveVector.y =
+                jumpspeed;
+            }
+        }
+        if (!controller.isGrounded)
+        {
+            //TODO: add logic so that top speed can't be made but it can be kept while in air, so that you get limited air control
+            MoveVector.x *= 0.85f;
+            MoveVector.z *= 0.85f;
+        }
+
+
+
+        MoveVector.y -= gravity * Time.deltaTime;
+        MoveVector = transform.TransformVector(MoveVector);
+        //TODO replace character controller
+        controller.Move(MoveVector * Time.deltaTime);
+    }
 
 
 
--- Assets/Scripts/Entity Scripts/EntityTemplate.cs
+++ Assets/Scripts/Entity Scripts/EntityTemplate.cs
@@ -10,6 +10,7 @@ using System;
 
 public abstract partial class ActiveEntity : Entity
 {
+    #region Partial Region
     protected partial void OnEntityEnter(Collider other);
      protected partial void OnEnemyStay(Collider other);
     protected partial void OnEntityExit(Collider other);
@@ -18,6 +19,8 @@ public abstract partial class ActiveEntity : Entity
     public partial bool isEntityNeutral(ActiveEntity other);
     public partial bool isEntityAlly(ActiveEntity other);
     public partial bool isEntityEnemy(ActiveEntity other);
+    protected partial void Movement(Vector3 Movement);
+    #endregion
     public enum TeamValue
     {
         Player1,
@@ -29,54 +32,6 @@ public abstract partial class ActiveEntity : Entity
 
     #endregion
 
-    #region Movement
-    public float testmovespeed = 12;
-    public float testjumpspeed = 20;
-    public string backwardaxis = "Vertical";
-    protected Vector3 MoveVector = Vector3.zero;
-    protected float currAcceleration;
-    public float gravity = 32.2F;
-    protected CharacterController controller;
-    protected float acclX;
-    protected float acclZ;
-    protected void Movement(Vector3 Movement)
-    {
-        //TODO: Use The entity's profile
-        float movespeed = testmovespeed;
-        float jumpspeed = testjumpspeed;
-        float backwardMovement = Input.GetAxisRaw(backwardaxis) == -1f ? 0.4f : 1f;
-
-        //side to side movement
-        MoveVector.x = Movement.x * movespeed;
-        //forward back movement
-        MoveVector.z = Movement.z * movespeed;
-
-        if (controller.isGrounded)
-        {
-            if (Movement.y > 0)
-            {
-                MoveVector.y =
-                jumpspeed;
-            }
-        }
-        if (!controller.isGrounded)
-        {
-            //TODO: add logic so that top speed can't be made but it can be kept while in air, so that you get limited air control
-            MoveVector.x *= 0.85f;
-            MoveVector.z *= 0.85f;
-        }
-
-
-
-        MoveVector.y -= gravity * Time.deltaTime;
-        MoveVector = transform.TransformVector(MoveVector);
-        //TODO replace character controller
-        controller.Move(MoveVector * Time.deltaTime);
-    }
-
-
-
-    #endregion
     protected virtual void Awake()
     {
 
